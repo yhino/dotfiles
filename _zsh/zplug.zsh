f74917a729280190195c11b90c1adc0c9cc07f87
Yoshiyuki HINO <yhinoz@gmail.com>
2018-04-12 00:23:09 +0900

    add zsh plugin

--- _zsh/zplug.zsh
+++ _zsh/zplug.zsh
@@ -4,6 +4,8 @@ zplug "~/.zsh", from:local, use:"<->_*.zsh"
 
 zplug "b4b4r07/enhancd", use:init.sh
 
+zplug "zsh-users/zsh-autosuggestions", hook-load:"ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'"
+
 zplug "zsh-users/zsh-completions"
 
 zplug "zsh-users/zsh-syntax-highlighting", defer:2, if:"is-at-least 4.3.11"
@@ -15,5 +17,9 @@ zplug "junegunn/fzf", as:command, use:bin/fzf-tmux
 zplug "stedolan/jq", as:command, from:gh-r, rename-to:jq
 
 zplug "github/hub", as:command, from:gh-r, rename-to:hub
+
+zplug "motemen/ghq", as:command, from:gh-r, rename-to:ghq
+
+zplug "tcnksm/ghr", as:command, from:gh-r, rename-to:ghr
 
 zplug "b4b4r07/zsh-gomi", as:command, use:bin/gomi
