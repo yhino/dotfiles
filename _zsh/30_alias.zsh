d9965e2ad0dbbc6b8fe1da7c235dbfd2fa02df7b
Yoshiyuki HINO <yhinoz@gmail.com>
2018-11-06 23:42:25 +0900

    revert alias mysql->mycli

--- _zsh/30_alias.zsh
+++ _zsh/30_alias.zsh
@@ -18,8 +18,6 @@ alias mv='mv -i'
 alias df='df -h'
 alias diff='diff -u'
 
-has 'mycli' && alias mysql='mycli'
-
 ssh() {
     if is_tmux_running; then
         local window_name=$(tmux display -p '#{window_name}')
