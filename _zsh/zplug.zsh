6e82907c48d43419dddc8ec309904e847426233f
Yoshiyuki HINO <yhinoz@gmail.com>
2021-03-09 14:04:08 +0900

    replace alternative `cd` (enhancd -> zoxide)

--- _zsh/zplug.zsh
+++ _zsh/zplug.zsh
@@ -1,9 +1,5 @@
 zplug "zplug/zplug", hook-build:"zplug --self-manage"
 
-zplug "~/.zsh", from:local, use:"<->_*.zsh"
-
-zplug "b4b4r07/enhancd", use:init.sh
-
 zplug "zsh-users/zsh-completions", defer:0
 
 zplug "zsh-users/zsh-autosuggestions", defer:2, hook-load:"ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'"
@@ -13,6 +9,11 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:3, if:"is-at-least 4.3.11"
 zplug "docker/cli", use:"contrib/completion/zsh", defer:1
 
 zplug "docker/compose", use:"contrib/completion/zsh", defer:1
+
+# disable because there is a problem that cd completion does not work
+#zplug "b4b4r07/enhancd", use:init.sh
+
+zplug "ajeetdsouza/zoxide", as:command, from:gh-r, hook-load:'eval \"\$(zoxide init zsh)\"'
 
 zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
 
@@ -25,12 +26,7 @@ zplug "tcnksm/ghr", as:command, from:gh-r, rename-to:ghr
 zplug "b4b4r07/zsh-gomi", as:command, use:bin/gomi
 
 zplug "plugins/kubectl", from:oh-my-zsh, lazy:true
-kubectl() {
-    unfunction kubectl
-    . "${ZPLUG_REPOS}/robbyrussell/oh-my-zsh/plugins/kubectl/kubectl.plugin.zsh"
-    kubectl "$@"
-}
 
-[[ -s ~/.kube/config ]] && {
-    zplug "superbrothers/zsh-kubectl-prompt"
-}
+zplug "superbrothers/zsh-kubectl-prompt", if:"[ -s ~/.kube/config ]"
+
+zplug "~/.zsh", from:local, use:"<->_*.zsh"
