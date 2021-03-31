2c0c32f6d597e5db9ff7f60733af1547b4b7ecee
Yoshiyuki HINO <yhinoz@gmail.com>
2021-03-31 15:21:00 +0900

    remove load secrets

--- _zsh/99_deinit.zsh
+++ _zsh/99_deinit.zsh
@@ -4,11 +4,3 @@ echo
 echo -ne "$fg_bold[cyan]This is ZSH $fg_bold[red]${ZSH_VERSION}$reset_color"
 echo -e  "$fg_bold[cyan] - DISPLAY on $fg_bold[red]${DISPLAY}$reset_color"
 echo
-
-() {
-    local f
-    for f in ./*secret*.zsh(N-.)
-    do
-        source $f
-    done
-}
