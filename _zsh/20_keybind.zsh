2b835759919d407947e1e75bcf973cd6ef9461a3
Yoshiyuki HINO <yhinoz@gmail.com>
2022-09-30 23:49:26 +0900

    :fire:  delete zsh keybind of history-beginning-search-*

--- _zsh/20_keybind.zsh
+++ _zsh/20_keybind.zsh
@@ -31,11 +31,6 @@ select-history() {
 zle -N select-history
 bindkey '^r' select-history
 
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "^P" history-beginning-search-backward-end
-bindkey "^N" history-beginning-search-forward-end
-
 fixssh() {
     if is_tmux_running; then
         for key in SSH_AUTH_SOCK; do
