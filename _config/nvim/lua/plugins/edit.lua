f6c0748057bc7148cc651646d36fe96599e83add
Yoshiyuki HINO <yhinoz@gmail.com>
2024-01-09 08:46:47 +0900

    :heavy_plus_sign: add plugin `vim-easy-align`

--- _config/nvim/lua/plugins/edit.lua
+++ _config/nvim/lua/plugins/edit.lua
@@ -36,4 +36,10 @@ return {
             }
         end,
     },
+    {
+        'junegunn/vim-easy-align',
+        keys = {
+            { 'ga', '<Plug>(EasyAlign)', mode = { 'n', 'x' } },
+        },
+    },
 }
