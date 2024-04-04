4d22cfddfd492461a620da11ac4baae23b80d2a7
Yoshiyuki HINO <yhinoz@gmail.com>
2024-04-04 15:07:20 +0900

    :sparkles: add plugin `mason`, `mason-lspconfig`

--- _config/nvim/lua/plugins/lsp.lua
+++ _config/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,70 @@
 return {
-    -- LSP
     {
-        'neovim/nvim-lspconfig',
+        'williamboman/mason.nvim',
+        config = function()
+            require('mason').setup()
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
         dependencies = {
+            'nvim-lspconfig',
             'b0o/schemastore.nvim',
             'Shougo/ddc-source-lsp',
         },
         config = function()
+            require('mason-lspconfig').setup()
+            require('mason-lspconfig').setup_handlers {
+                function (server_name)
+                    local capabilities = require("ddc_source_lsp").make_client_capabilities()
+                    require('lspconfig')[server_name].setup {
+                        capabilities = capabilities,
+                    }
+                end,
+                ["jsonls"] = function ()
+                    require('lspconfig').jsonls.setup {
+                        settings = {
+                            json = {
+                                schemas = require('schemastore').json.schemas(),
+                                validate = { enable =true },
+                            },
+                        },
+                    }
+                end,
+                ["yamlls"] = function ()
+                    require('lspconfig').yamlls.setup {
+                        settings = {
+                            json = {
+                                schemas = require('schemastore').json.schemas(),
+                                validate = { enable =true },
+                            },
+                        },
+                    }
+                end,
+            }
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
             local lspconfig = require('lspconfig')
             local capabilities = require("ddc_source_lsp").make_client_capabilities()
-            lspconfig.gopls.setup({
+            lspconfig.gopls.setup{
                 capabilities = capabilities,
-            })
-            lspconfig.pyright.setup({
+            }
+            lspconfig.pyright.setup{
                 capabilities = capabilities,
-            })
+            }
             lspconfig.jsonls.setup {
+                capabilities = capabilities,
+                settings = {
+                    json = {
+                        schemas = require('schemastore').json.schemas(),
+                        validate = { enable =true },
+                    },
+                },
+            }
+            lspconfig.yamlls.setup {
                 capabilities = capabilities,
                 settings = {
                     json = {
