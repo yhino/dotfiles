7d20c616335d1dff6c559b7d285185b98b2d6af0
Yoshiyuki HINO <yhinoz@gmail.com>
2026-04-04 16:38:31 +0900

    :arrow_up: upgrade telescope to latest

--- _config/nvim/lua/plugins/telescope.lua
+++ _config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,10 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
         dependencies = {
             'nvim-lua/plenary.nvim',
+            {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+            'nvim-telescope/telescope-file-browser.nvim',
         },
         keys = {
                 {'<leader>ft', '<cmd>Telescope<cr>'},
