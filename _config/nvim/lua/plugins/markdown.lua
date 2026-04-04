77665395fddb478dd768b2542261b74f71ef0367
Yoshiyuki HINO <yhinoz@gmail.com>
2026-04-04 16:25:29 +0900

    :wrench: disabled bullet icon for render-markdown.nvim

--- _config/nvim/lua/plugins/markdown.lua
+++ _config/nvim/lua/plugins/markdown.lua
@@ -11,6 +11,9 @@ return {
                 width = 'full',
                 icons = {},
             },
+            bullet = {
+                icons = {},
+            },
         },
     },
     {
